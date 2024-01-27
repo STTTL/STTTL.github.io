7bbb3f797a61e744818f75c801af3c76c1e49b88
STTTL <157855426+STTTL@users.noreply.github.com>
2024-01-26 19:16:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function displayName() {
+        var name = document.getElementById('nameInput').value;
+        document.getElementById('nameDisplay').innerText = 'Hello ' + name + ', welcome to my site!';
+    }
